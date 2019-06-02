fb61be6ab3ae3ebc8b093ab275d85b2c77054d03
lkullez <linus.kulle@gmail.com>
2019-06-02 20:46:20 +0200

    lagt till komponenter för boklista och böcker

--- js-projekt-flerplatformasapplikationer/src/app/app.module.ts
+++ js-projekt-flerplatformasapplikationer/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule
