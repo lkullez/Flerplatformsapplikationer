82ae874c0f0e7e66c572a0583286eb0c65843fe3
lkullez <linus.kulle@gmail.com>
2019-06-02 19:39:44 +0200

    bugg-fix

--- js-projekt-flerplatformasapplikationer/src/app/app.component.ts
+++ js-projekt-flerplatformasapplikationer/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'js-projekt-flerplatformasapplikationer';
+  title = 'DA344A';
 }
