6ac8dc86c620b124c86bb635159e770702600c84
lkullez <linus.kulle@gmail.com>
2019-06-03 19:40:25 +0200

    laggt till kod för att hämta api data

--- js-projekt-flerplatformasapplikationer/src/app/app.component.ts
+++ js-projekt-flerplatformasapplikationer/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DA344A';
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(){
+  let obs = this.http.get('https://sv443.net/jokeapi/category/Miscellaneous?blacklistFlags=nsfw');
+  obs.subscribe((response) => console.log(response));
+  }
 }
