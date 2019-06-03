09a42c2e64371643a59948dd21226a14d1eaa779
lkullez <linus.kulle@gmail.com>
2019-06-03 21:03:26 +0200

    api kod för rules och ta emot samt visa JSON data

--- js-projekt-flerplatformasapplikationer/src/app/app.module.ts
+++ js-projekt-flerplatformasapplikationer/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { JokeFormComponent } from './joke-form/joke-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    JokeFormComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ js-projekt-flerplatformasapplikationer/src/app/joke-form/joke-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Component({
+  selector: 'app-joke-form',
+  templateUrl: './joke-form.component.html',
+  styleUrls: ['./joke-form.component.css']
+})
+export class JokeFormComponent implements OnInit {
+
+  category: string = "";
+  blacklist: string = "";
+  response: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  search(){
+    let data = this.http.get('https://sv443.net/jokeapi/category/' + this.category + '?blacklistFlags=' + this.blacklist);
+    data.subscribe((response) => {
+      this.response = response;
+      console.log(this.response)
+    })
+  }
+
+}
